40280fcb6dcdd1193b1445ec8d6a36818608b205
RossT <rthomson412@gmail.com>
2022-03-10 10:48:20 -0500

    HTML working

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+    console.log("HTML works; button is working");
+
+    return "PLACEHOLDER: generated password.;"
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
